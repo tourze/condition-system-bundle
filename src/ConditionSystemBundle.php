eb0f28fcbe11944f1fcd3144f44886373ad60243
github-ci <you@example.com>
2025-11-03 14:18:30 +0800

    update++++++++++++

--- src/ConditionSystemBundle.php
+++ src/ConditionSystemBundle.php
@@ -5,6 +5,7 @@ namespace Tourze\ConditionSystemBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class ConditionSystemBundle extends Bundle implements BundleDependencyInterface
 {
@@ -12,6 +13,7 @@ class ConditionSystemBundle extends Bundle implements BundleDependencyInterface
     {
         return [
             DoctrineBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
